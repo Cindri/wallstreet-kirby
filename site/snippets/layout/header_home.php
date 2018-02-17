cde80e1e1f2c2bef6eefe864c9f2044908860d0f
David <davidpeter1337@gmail.com>
2018-02-17 17:19:11 +0100

    Bild-Pfade im Home-Header anpassen

--- site/snippets/layout/header_home.php
+++ site/snippets/layout/header_home.php
@@ -22,8 +22,7 @@
 
                     <!-- Logo -->
                     <div id="logo" class="col-sm-4 logo col-sm-pull-4">
-                        <a href="index.html"><img alt="The Retation"
-                                                  src="../assets/imgs/logo.png"></a>
+                        <a href="index.html"><img alt="The Retation" src="assets/imgs/logo.png"></a>
                     </div>
 
                 </div>
@@ -39,18 +38,18 @@
     <div id="banner">
         <div class="slider-wrapper">
             <div class="nivoSlider">
-                <img src="../images/home/slider/slider-1.jpg" alt=""
-                     title="#nivoCaption-1" /> <img src="../images/home/slider/slider-2.jpg"
+                <img src="assets/images/home/slider/slider-1.jpg" alt=""
+                     title="#nivoCaption-1" /> <img src="assets/images/home/slider/slider-2.jpg"
                                                     alt="" title="#nivoCaption-2" /> <img
-                    src="../images/home/slider/slider-3.jpg" alt="" title="" />
+                    src="assets/images/home/slider/slider-3.jpg" alt="" title="" />
             </div>
             <div id="nivoCaption-1" class="nivo-html-caption">
                 <div class="nivo-caption-img-1 animated animation-delay-50"
                      data-animate="fadeInUp">
-                    <img alt="" src="../images/home/slider/img-1.png">
+                    <img alt="" src="assets/images/home/slider/img-1.png">
                 </div>
                 <div class="nivo-caption-img-2 animated" data-animate="fadeInUp">
-                    <img alt="" src="../images/home/slider/img-2.png">
+                    <img alt="" src="assets/images/home/slider/img-2.png">
                 </div>
                 <div class="nivo-caption-content">
                     <h1 class="line-title title animated animation-delay-75"
@@ -64,11 +63,11 @@
             </div>
             <div id="nivoCaption-2" class="nivo-html-caption">
                 <div class="nivo-caption-img-1 animated" data-animate="fadeInUp">
-                    <img alt="" src="../images/home/slider/img-3.png">
+                    <img alt="" src="assets/images/home/slider/img-3.png">
                 </div>
                 <div class="nivo-caption-img-2 animated animation-delay-50"
                      data-animate="fadeInUp">
-                    <img alt="" src="../images/home/slider/img-4.png">
+                    <img alt="" src="assets/images/home/slider/img-4.png">
                 </div>
                 <div class="nivo-caption-content">
                     <h1 class="line-title title animated animation-delay-75"
