64f5ae100fa72c41929f717182e7714ee537096a
David <davidpeter1337@gmail.com>
2018-02-18 14:19:31 +0100

    Default-Template und Optionen Gallery-Blueprint

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,19 +1,35 @@
-<?php snippet('header') ?>
+<!doctype html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="">
+<![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang="">
+<![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang="">
+<![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
 
-  <main class="main" role="main">
+<?= snippet('layout/head') ?>
 
-    <header class="wrap">
-      <h1><?= $page->title()->html() ?></h1>
-      <div class="intro text">
-        <?= $page->intro()->kirbytext() ?>
-      </div>
-      <hr />
-    </header>
-      
-    <div class="text wrap">
-      <?= $page->text()->kirbytext() ?>
+<body class="loadpage home style-v1">
+<div id="wrap">
+
+    <?= snippet('layout/header_home') ?>
+
+    <div class="main-content" id="main-content">
+
+        <h1><?= $page->title() ?></h1>
+
     </div>
 
-  </main>
+    <?= snippet('layout/footer') ?>
 
-<?php snippet('footer') ?>
+</div>
+
+<?= snippet('layout/jsAfterWrapper') ?>
+
+</body>
+</html>
