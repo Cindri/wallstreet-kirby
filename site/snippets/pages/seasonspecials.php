fc75bf7b0fa8815e4d6ad8ba78c01ac2368ceafa
David <davidpeter1337@gmail.com>
2018-03-02 21:17:21 +0100

    Season Specials anlegen

---
+++ site/snippets/pages/seasonspecials.php
@@ -0,0 +1,6 @@
+<div class="page-header">
+    <figure class="post-thumbnail">
+        <img alt="" src="images/about/about.jpg">
+    </figure>
+    <h1 class="title"><span class="line-title">About Us<i class="fa"></i></span></h1>
+</div>
