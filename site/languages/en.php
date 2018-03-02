f710338fa021b6a7f3b1002a62ae9df2f6c9010b
David <davidpeter1337@gmail.com>
2018-03-02 21:20:32 +0100

    Übersetzungen

--- site/languages/en.php
+++ site/languages/en.php
@@ -1,19 +1,23 @@
 <?php
 
-l::set('date.format',            'F jS, Y');
-l::set('footer.made-with-kirby', 'Made with Kirby and <b class="heart">♥</b>');
+//l::set('date.format',            'F jS, Y');
+//l::set('footer.made-with-kirby', 'Made with Kirby and <b class="heart">♥</b>');
+//
+//l::set('home.projects.latest',   'Latest Projects');
+//l::set('home.projects.more',     'show all projects &hellip;');
+//
+//l::set('blog.more',              'read more');
+//l::set('blog.no-articles',       'This blog does not contain any articles yet.');
+//
+//l::set('contact.get-in-touch',   'Get in Touch');
+//
+//l::set('about.team',             'Our Purring Team');
+//l::set('about.email',            'Email:');
+//l::set('about.phone',            'Phone:');
+//
+//l::set('pagination.prev',       'newer articles');
+//l::set('pagination.next',        'older articles');
 
-l::set('home.projects.latest',   'Latest Projects');
-l::set('home.projects.more',     'show all projects &hellip;');
+l::set('opening_hours', 'Opening hours');
 
-l::set('blog.more',              'read more');
-l::set('blog.no-articles',       'This blog does not contain any articles yet.');
-
-l::set('contact.get-in-touch',   'Get in Touch');
-
-l::set('about.team',             'Our Purring Team');
-l::set('about.email',            'Email:');
-l::set('about.phone',            'Phone:');
-
-l::set('pagination.prev',       'newer articles');
-l::set('pagination.next',        'older articles');
+l::set('all', 'All');
