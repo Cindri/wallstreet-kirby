3e40b097300501b7fad848137b9369c68246eb6a
David <davidpeter1337@gmail.com>
2018-03-09 12:18:50 +0100

    Bild in Season Specials

--- site/snippets/pages/seasonspecials.php
+++ site/snippets/pages/seasonspecials.php
@@ -1,6 +1,8 @@
 <div class="page-header">
-    <figure class="post-thumbnail">
-        <img alt="" src="images/about/about.jpg">
-    </figure>
-    <h1 class="title"><span class="line-title">About Us<i class="fa"></i></span></h1>
+    <?php if ($image = $page->main_image()->toFile()): ?>
+        <figure class="post-thumbnail">
+            <img src="<?= $image->url() ?>" />
+        </figure>
+    <?php endif ?>
+    <h1 class="title"><span class="line-title"><?= $page->title() ?><i class="fa"></i></span></h1>
 </div>
