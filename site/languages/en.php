7b08ef6a2bd627323876f47c47a539fe40b1d8d5
David <davidpeter1337@gmail.com>
2018-03-23 12:29:02 +0100

    Statische Übersetzungen pflegen

--- site/languages/en.php
+++ site/languages/en.php
@@ -19,3 +19,15 @@ l::set('thursday', 'Thursday');
 l::set('friday', 'Friday');
 l::set('saturday', 'Saturday');
 l::set('sunday', 'Sunday');
+
+
+l::set('newsletter_invitation_short', 'Always up-to-date! Here you can register at our Wallstreet newsletter system:');
+l::set('newsletter_invitation_long', 'Immer auf dem neusten Stand! Gerne senden wir Ihnen kostenlos immer Sonntags unsere Powerlunch-Angebote für die kommende Woche.
+Alle 6-8 Wochen erhalten Sie auch die neusten Season Specials! Und über Veranstaltungen im Wallstreet informieren wir Sie frühzeitig.
+Einfach unten eintragen und "Eintragen"-Button drücken. Sie erhalten eine Mail bzw. bei Fax-Service einen Anruf mit der Bitte um Bestätigung. Sie können Sich jederzeit wieder aus dem Verteiler austragen.');
+l::set('newsletter_link_long_registration', 'Advanced registration');
+l::set('error_newsletter_requirements', 'E-mail or fax are required fields for the newsletter registration. Without those contact data, we can not send you any information. If you wish to get our mails via fax, we also need a phone number for verification.');
+l::set('newsletter_registration_success', 'Thank you! Your registration at the newsletter system was successful! Please confirm your registration by following the link in the confirmation E-mail you just recieved.');
+l::set('newsletter_registration_fail', 'Unfortunately, your registration process could not be finished. Please try again later.');
+
+l::set('copyright_notice', 'Wallstreet Hamilton Baden-Baden. All rights reserved.');
